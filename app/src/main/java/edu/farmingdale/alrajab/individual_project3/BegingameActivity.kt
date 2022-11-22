1c7ff99b24c92bde71a6edfc1d3e08f9603e4c57
Murad Khan <Muradak1234@gmail.com>
2022-11-22 10:55:03 -0500

    add a begingameactivity

---
+++ app/src/main/java/edu/farmingdale/alrajab/individual_project3/BegingameActivity.kt
@@ -0,0 +1,22 @@
+package edu.farmingdale.alrajab.individual_project3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class BegingameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_begingame)
+       val easyLevelBtn= findViewById<Button>(R.id.easylevelbutton)
+        easyLevelBtn.setOnClickListener {
+
+
+            val easyLevelIntent = Intent(this, EasylevelActivity::class.java)
+            startActivity(
+                easyLevelIntent
+            )
+        }
+    }
+}
